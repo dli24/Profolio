22e0852cee98ec2036b7d6d836ac27e8b5fce934
dli24 <dli24@buffalo.edu>
2019-08-24 13:35:54 -0400

    all initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function time(){
+    var d = new Date();
+    var s = d.getSeconds();
+    var m = d.getMinutes();
+    var h = d.getHours();
+    // document.querySelector('.display').innerHTML = (h + ":" + m + ":" + s);
+}
+
+// setInterval(time,1000);
